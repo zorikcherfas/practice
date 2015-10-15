d8826b80573e00500e29f33cc6bc6b14258d148c
Zorik Cherfas <zorik2k@Zoriks-MacBook-Pro.local>
2015-10-16 01:42:06 +0300

    Adding SinglyList, simple chain

---
+++ linked_list/SinglyLinkedLists.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+class SinglyLinkedList
+{
+
+public:
+    SinglyLinkedList( int value ): next( NULL ), data( value ) {}
+    ~SinglyLinkedList() {};
+
+    SinglyLinkedList *getNext() const { return next; }
+    int getValue() const { return data; }
+    void setNext( SinglyLinkedList *elem ) { next = elem; }
+    void setValue( int value ) { data = value; }
+
+  private:
+    SinglyLinkedList *next;
+    int         data;
+
+};
+
+int main()
+{	
+	cout << "Testing class SinglyLinkedList" <<endl;
+	SinglyLinkedList *list1 = new SinglyLinkedList(10);
+	cout << "Data in the list1 is " << list1->getValue() <<endl;
+	
+	SinglyLinkedList *list2 = new SinglyLinkedList(30);
+	cout << "Data in the list2 is " << list2->getValue() <<endl;
+
+	SinglyLinkedList *list3 = new SinglyLinkedList(50);
+	cout << "Data in the list3 is " << list3->getValue() <<endl;
+
+	cout << "Making chain of list" <<endl;
+
+	list1->setNext(list2);
+	list2->setNext(list3);
+
+	cout << "value list 1:  " << list1->getValue() <<endl ;
+	cout << "value list 2:  " << list1->getNext()->getValue() <<endl ;
+	cout << "value list 3:  " << list1->getNext()->getNext()->getValue() <<endl;
+}
